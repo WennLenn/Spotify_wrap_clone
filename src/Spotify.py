943891c691604b13cd875033e09e8ea7509ad776
WennLenn <lennardprof@gmail.com>
2026-06-10 11:12:58 +0200

    Implement Flask app to display top 10 artists; add data handling in Spotify.py and update index.html for dynamic rendering

--- src/Spotify.py
+++ src/Spotify.py
@@ -1,10 +1,13 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
+from app import app
 
+# load client data
 with open("client.json", "r", encoding="utf-8") as f:
     client = json.load(f)
 
+# set the information
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id= client["ID"],
     client_secret= client["secret"],
@@ -12,16 +15,19 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     scope="user-top-read"
 ))
 
+# load spotidata json file
 with open("spotidata.json", "r") as f:
     spotidata = json.load(f)
 
-
+# request top 10 artists
 results = sp.current_user_top_artists(limit=10)
 for idx, artist in enumerate(results["items"], 1):
-    top_art_arr = spotidata["top_art"] #(ich)
+    top_art_arr = spotidata["top_art"] 
     top_art_arr.insert(idx, artist["name"])
 
-
+# save the top 10 to a json file
 with open("spotidata.json", "w",) as f:
     spotidata["top_art"] = top_art_arr
     json.dump(spotidata, f, indent=2)
+    
+app.run()
